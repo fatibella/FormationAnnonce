d8354f47364c3a195c87474ca6bc84e8dc3b435e
DELL <DELL@DESKTOP-OT04KG3>
2020-02-21 21:35:57 +0100

    H

---
+++ projetFormationn/src/main/java/ma/fst/demo/ProjetFormationnApplication.java
@@ -0,0 +1,13 @@
+package ma.fst.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFormationnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFormationnApplication.class, args);
+	}
+
+}
---
+++ projetFormationn/src/test/java/ma/fst/demo/ProjetFormationnApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fst.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFormationnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
