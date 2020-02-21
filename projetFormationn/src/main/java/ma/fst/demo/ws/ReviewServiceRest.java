f05773fe6c41498bedf78cbe4f64ab16fe265993
DELL <DELL@DESKTOP-OT04KG3>
2020-02-21 22:12:02 +0100

    ws Rests

---
+++ projetFormationn/src/main/java/ma/fst/demo/ws/ReviewServiceRest.java
@@ -0,0 +1,10 @@
+package ma.fst.demo.ws;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ReviewServiceRest {
+
+}
